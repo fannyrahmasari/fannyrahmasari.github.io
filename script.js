b4d34a47b52e4e6f9e5fcc6fe0f96bba11eaa55e
Fanny Rahmasari <96949428+fannyrahmasari@users.noreply.github.com>
2022-10-30 21:11:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(document).ready(function () {
   var currentSection = 0;
   $('#content').hide();
@@ -23,29 +23,3 @@ $(document).ready(function () {
     return false;
   });
 });
-=======
-$(document).ready(function () {
-  var currentSection = 0;
-  $('#content').hide();
-  $('ul li a').click(function () {
-    $('ul li a').removeClass('active');
-    $(this).addClass('active');
-
-    var href = $(this).attr('href');
-    $('#isicontent>div').hide();
-
-    $(href).show();
-
-    if (currentSection == 0) {
-      $('#content').slideToggle(200);
-      currentSection = href;
-    } else if (currentSection == href) {
-      $('#content').slideToggle(200);
-      currentSection = 0;
-    } else {
-      currentSection = href;
-    }
-    return false;
-  });
-});
->>>>>>> 63981250649070da5c36b739d026c89c53cf2582
